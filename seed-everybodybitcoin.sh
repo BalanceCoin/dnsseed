79aa1371c587953000c79d0ff17a2062b67a825c
2blon9 <2blon9@gmail.com>
2018-02-13 18:21:36 +0800

    Rename seed2.

--- seed-everybodybitcoin.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./dnsseed -h seed.everybodybitcoin.org -n ns1.everybodybitcoin.org -m seeder@everybodybitcoin.org
---
+++ seed2-everybodybitcoin.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./dnsseed -h seed.everybodybitcoin.org -n ns2.everybodybitcoin.org -m seeder@everybodybitcoin.org
